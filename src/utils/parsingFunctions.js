029a6118d108698ddd3fee2eb360fa874df62e90
Dawid Zyluk <dawid@typoedition.com>
2023-03-14 16:42:29 +0100

    maybe the last time I think the postfix representation is working

--- src/utils/parsingFunctions.js
+++ src/utils/parsingFunctions.js
@@ -24,40 +24,32 @@ export function toInfix(expression) {
   return output;
 }
 
+
+//1+2*3-4
+// 1 2 3 * + 4 -
 export function toPostfix(expression) {
   let output = [];
   let stack = [];
-  let numberString = "";
   for (let char of expression) {
-    if ((char >= "0" && char <= "9") || char === "." || char === ",")
-      numberString += char;
+    if (char >= "0" && char <= "9") output.push(char);
     if (precedence.has(char)) {
-      if (numberString.length > 0) {
-        output.push(numberString);
-        numberString = "";
-      }
       while (
+        stack[stack.length - 1] !== undefined &&
         stack[stack.length - 1] !== "(" &&
-        precedence.get(stack[stack.length - 1]) > precedence.get(char)
+        precedence.get(stack[stack.length - 1]) >= precedence.get(char)
       ) {
         output.push(stack.pop());
       }
-      // if(char !== ')') stack.push(char);
       stack.push(char);
     }
-    if (char === "(") stack.push(char);
-    if (char === ")") {
-      while (stack[stack.length - 1] != "(" || stack.length === 0)
-        output.push(stack.pop());
-        if(stack[stack.length - 1] === '(') stack.pop();
+    if(char === '(') stack.push(char)
+    if(char === ')') {
+      while(stack[stack.length - 1] !== "(" && stack.length > 0){
+        output.push(stack.pop())
+      }
+      stack.pop();
     }
-
-    // 4/(2+2)*2
-    // 4 2 2 + / 2 *
   }
-  if (numberString !== "") output.push(numberString);
   while (stack.length > 0) output.push(stack.pop());
-
   return output;
 }
-
